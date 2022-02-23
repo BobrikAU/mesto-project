985153551877d32516a8a7990929c8d04ee262f0
Aleksandr Bobrik <kirbob0@yandex.ru>
2022-02-23 18:11:52 +0300

    сделано шесть карточек и разметка попапа добавления карточек

--- index.js
+++ index.js
@@ -4,9 +4,39 @@ const profilePopup = document.querySelector('.popup'); //диалоговое о
 const closeButton = document.querySelector('.popup__close-button'); //кнопка закрытия диалогового окна
 const profileName = document.querySelector('.profile__title'); //заголовок с именем пользователя в профиле
 const popupProfileName = document.querySelector('[name=name]'); //поле для введения имени пользователя в модальном окне редактирования профиля
-const ProfileSelf = document.querySelector('.profile__subtitle'); //подзаголовок с информацией о пользователе в профиле
+const profileSelf = document.querySelector('.profile__subtitle'); //подзаголовок с информацией о пользователе в профиле
 const popupProfileSelf = document.querySelector('[name=about-self]'); //поле для информации о пользователе в модальном окне редактирования профиля
 const formElement = document.querySelector('.popup__form'); //форма отправки данных для профиля пользователя
+const emptyCard = document.querySelector('#empty-item').content; // заготовка верстки карточки с фотографией
+const photosList = document.querySelector('.photos__list'); // список карточек с фотографиями
+
+// фотографии для карточек при загрузке
+const photosCards = [
+  {
+    name : 'Летающий хищник',
+    link : 'https://i.postimg.cc/4x0RGsq8/sammy-wong-96qgwu-Zu-PJs-unsplash.jpg',
+  },
+  {
+    name : 'Птица',
+    link : 'https://i.postimg.cc/pL09fh7s/cody-west-fx-OE7y8-Hm-Q8-unsplash-1.jpg',
+  },
+  {
+    name : 'Красота волн',
+    link : 'https://i.postimg.cc/TYdYt2nF/bernd-dittrich-db-51-OUU-m8-unsplash-1.jpg',
+  },
+  {
+    name : 'Могучий кит',
+    link : 'https://i.postimg.cc/VvY1Z40n/max-lissenden-sn-YLMKph-Cf4-unsplash-1.jpg',
+  },
+  {
+    name : 'Энергия природы',
+    link : 'https://i.postimg.cc/mrQvxCsw/navi-jtyjc-Qj6-Ml-A-unsplash-1.jpg',
+  },
+  {
+    name : 'Рысь',
+    link : 'https://i.postimg.cc/xTcBfWQq/mario-esposito-9-E2-Aztou-Xe-M-unsplash-1.jpg',
+  }
+];
 
 //обработчик закрытия модального окна
 function closePopup() {
@@ -17,7 +47,7 @@ function closePopup() {
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profileName.textContent = popupProfileName.value;
-  ProfileSelf.textContent = popupProfileSelf.value;
+  profileSelf.textContent = popupProfileSelf.value;
   closePopup();
 }
 
@@ -26,7 +56,7 @@ editButton.addEventListener('click', function() {
   popupProfileName.removeAttribute('placeholder');
   popupProfileName.setAttribute('value', profileName.textContent);
   popupProfileSelf.removeAttribute('placeholder');
-  popupProfileSelf.setAttribute('value', ProfileSelf.textContent);
+  popupProfileSelf.setAttribute('value', profileSelf.textContent);
   profilePopup.className = 'popup popup_opened';
 });
 
@@ -35,5 +65,15 @@ closeButton.addEventListener('click', closePopup);
 
 //сохранение изменений в профиле пользователя из формы
 formElement.addEventListener('submit', formSubmitHandler);
+
+// автоматическая загрузка карточек с фотографиями при старте
+for (let i = 0; i < 6; i++) {
+  const card = emptyCard.cloneNode(true);
+  card.querySelector('.photos__photo').src = photosCards[i].link;
+  card.querySelector('.photos__photo').alt = photosCards[i].name;
+  card.querySelector('.photos__caption').textContent = photosCards[i].name;
+  photosList.append(card);
+}
+
 
 
